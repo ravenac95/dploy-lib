f62f3c10179fd484a3b9f7a46be62d39ceff2efe
Reuven V. Gonzales <reuven@tobetter.us>
2012-08-09 10:36:35 -0700

    More work on services

--- dploylib/services/service.py
+++ dploylib/services/service.py
@@ -6,25 +6,57 @@ class ConfigMapper(object):
     pass
 
 
+class ServerConfig(object):
+    def __init__(self):
+        self._servers = {}
+
+    def apply_blueprints(self, blueprints):
+        """Apply's the blueprints and verify the settings"""
+        pass
+
+    def __getitem__(self, name):
+        return self._servers[name]
+
+    def __setitem__(self, name, server):
+        self._servers[name] = server
+
+
 class Service(object):
     """A facade to a DployService"""
     def __init__(self, extends=None, config_mapper=None, coordinator=None):
-        self._extends = extends or []
-        self._server_classes = {}
+        self._blueprints = extends or []
+        self._server_config = ServerConfig()
+        self._configuration_locked = False
         self._config_mapper = config_mapper or ConfigMapper()
         self._coordinator = coordinator or ThreadedServerCoordinator()
 
     def add_server(self, name, server_cls):
-        self._server_classes[name] = server_cls
+        self._server_config[name] = server_cls
 
-    def process_blueprints(self):
-        pass
+    def _apply_blueprints(self):
+        self._server_config.apply_blueprints(self._blueprints)
+        self._configuration_locked = True
+
+    @property
+    def server_config(self):
+        if not self._configuration_locked:
+            self._apply_blueprints()
+            return self._server_config
+        return self._server_config
 
     def start(self, config):
         """Starts the service with a certain configuration"""
         config_mapper = self._config_mapper
-        server_classes = self._server_classes
         coordinator = self._coordinator
+        server_config = self.server_config
 
-        server_names = server_classes.keys()
-        config_mapper.process(config, servers=server_names)
+        server_names = server_config.names()
+        processed_config = config_mapper.process(config, servers=server_names)
+        coordinator.setup_servers(server_config, processed_config)
+        coordinator.start()
+
+    def wait(self):
+        self._coordinator.wait()
+
+    def stop(self):
+        self._coordinator.stop()
--- tests/services/test_service.py
+++ tests/services/test_service.py
@@ -13,29 +13,41 @@ def test_initialize_service():
     mock_server = Mock()
     mock_config_mapper = Mock()
 
-    service = Service(extends=[mock_blueprint], config_mapper=mock_config_mapper)
+    service = Service(extends=[mock_blueprint],
+            config_mapper=mock_config_mapper)
     service.add_server('test', mock_server)
 
 
 class TestBasicService(object):
     def setup(self):
         self.mock_blueprint = Mock()
+        self.mock_blueprints = [self.mock_blueprint]
         self.mock_server = Mock()
         self.mock_config_mapper = Mock()
+        self.mock_coordinator = Mock()
 
         service = Service(extends=[self.mock_blueprint],
-                config_mapper=self.mock_config_mapper)
+                config_mapper=self.mock_config_mapper,
+                coordinator=self.mock_coordinator)
         service.add_server('test', self.mock_server)
         self.service = service
 
     def test_start(self):
+        mock_coordinator = self.mock_coordinator
         fake_config = {
             'service': {
                 'servers': {'test': 'testconfig'},
             },
         }
-        mock_process_blueprints = self.service.process_blueprints = Mock()
+        mock_server_config = self.service._server_config = Mock()
         self.service.start(fake_config)
 
-        self.mock_config_mapper.process.assert_called_with(fake_config, servers=['test'])
-        self.mock_coordinator
+        mock_server_config.apply_blueprints.assert_called_with(
+                self.mock_blueprints)
+
+        self.mock_config_mapper.process.assert_called_with(fake_config,
+                servers=mock_server_config.names.return_value)
+        mock_processed_config = self.mock_config_mapper.process.return_value
+        mock_coordinator.setup_servers.assert_called_with(
+                mock_server_config, mock_processed_config)
+        mock_coordinator.start.assert_called_with()
