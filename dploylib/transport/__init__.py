3bef3a47ace4ad6f2715ca0dc27968c3a4658043
Reuven V. Gonzales <reuven@tobetter.us>
2012-08-14 18:21:10 -0700

    Many more docs for the transport package

--- dploylib/transport/__init__.py
+++ dploylib/transport/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from wrapper import *
 from envelope import *
 from poll import *
--- dploylib/transport/envelope.py
+++ dploylib/transport/envelope.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+dploylib.transport.envelope
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Defines the envelope. The envelope is how all of the communication occurs
+between dploy services. This does not include dploy web services. Those differ
+from zeromq based services.
+"""
+
+
 class Envelope(object):
     """Dploy's message envelope.
 
@@ -17,14 +29,31 @@ class Envelope(object):
         The ``id`` portion of the envelope may seem like unnecessary data, but
         it allows the envelope to be used in pub-sub effectively.
 
-
+    :param id: A string id for the envelope
+    :type id: str
+    :param mimetype: The mimetype for the envelope
+    :type mimetype: str
+    :param data: The envelope's body
     """
     @classmethod
     def new(cls, mimetype, data, id=''):
+        """Create a new envelope. This is the preferred way to create a new
+        envelope.
+
+        :param mimetype: The mimetype for the envelope
+        :param data: The envelope's body
+        :param id: (optional) A string id for the envelope. Defaults to ''
+        """
         return cls(id, mimetype, data)
 
     @classmethod
     def from_raw(cls, raw):
+        """Creates an envelope from a tuple or list
+
+        :param raw: Raw data for an envelope
+        :type raw: tuple or list
+        """
+
         id, mimetype, data = raw
         return cls(id, mimetype, data)
 
@@ -46,8 +75,9 @@ class Envelope(object):
         return self._data
 
     def transfer_object(self):
-        """This is the object to be send over the wire.
+        """This is the object to be sent over the wire. The reverse of this is
+        Envelope.from_raw
 
-        For zmq this should be an array
+        For zmq this should be an list
         """
         return [self._id, self._mimetype, self._data]
